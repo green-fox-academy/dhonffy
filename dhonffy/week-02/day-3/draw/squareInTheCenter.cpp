a9f8a1f30d0f28d5120c5289718fdb44cf5f7a2b
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-03 14:00:22 +0200

    correct square in the center

--- dhonffy/week-02/day-3/draw/squareInTheCenter.cpp
+++ dhonffy/week-02/day-3/draw/squareInTheCenter.cpp
@@ -4,9 +4,7 @@ void draw(SDL_Renderer *gRenderer)
 {
     // Draw a green 100x100 square to the canvas' center.
     SDL_SetRenderDrawColor(gRenderer, 0x00 /*R*/, 0xFF /*G*/, 0x00 /*B*/, 0xFF /*A*/);
-    SDL_RenderDrawLine(gRenderer, 270, 190, 270, 290);
-    SDL_RenderDrawLine(gRenderer, 270, 190, 370, 190);
-    SDL_RenderDrawLine(gRenderer, 370, 190, 370, 290);
-    SDL_RenderDrawLine(gRenderer, 370, 290, 270, 290);
+    SDL_Rect fillRect = {270, 190, 100, 100};
+    SDL_RenderFillRect( gRenderer, &fillRect );
 }
 
