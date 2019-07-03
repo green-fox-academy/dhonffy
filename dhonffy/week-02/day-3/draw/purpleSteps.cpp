da6a4dfb413f88eb65a66d212e8f25328099798b
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-03 16:37:10 +0200

    do outlines purple steps

--- dhonffy/week-02/day-3/draw/purpleSteps.cpp
+++ dhonffy/week-02/day-3/draw/purpleSteps.cpp
@@ -2,22 +2,24 @@
 #include <iostream>
 #include <vector>
 
-void drawSteps(SDL_Renderer*, std::vector<int>);
+void drawSteps(SDL_Renderer*);
 
 void draw(SDL_Renderer *gRenderer)
 {
     // Reproduce this:
     // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
     // Pay attention for the outlines as well
-    std::vector<int> colors = {138, 43, 226};
-    drawSteps(gRenderer, colors);
+    drawSteps(gRenderer);
 }
 
-void drawSteps(SDL_Renderer *gRenderer, std::vector<int> colors)
+void drawSteps(SDL_Renderer *gRenderer)
 {
     for (int i = 0; i < 190; i += 10) {
-        SDL_SetRenderDrawColor(gRenderer, colors[0] /*R*/, colors[1] /*G*/, colors[2] /*B*/, 0x00 /*A*/);
-        SDL_Rect fillRect = {10 + i, 10 + i , 10, 10};
+        SDL_SetRenderDrawColor(gRenderer, 138 /*R*/, 43 /*G*/, 226 /*B*/, 0x00 /*A*/);
+        SDL_Rect fillRect = {11 + i, 11 + i , 8, 8};
         SDL_RenderFillRect(gRenderer, &fillRect);
+        SDL_SetRenderDrawColor(gRenderer, 0x00 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0x00 /*A*/);
+        SDL_Rect fillRectOutline = {10 + i, 10 + i , 10, 10};
+        SDL_RenderDrawRect(gRenderer, &fillRectOutline);
     }
 }
