9bccf33fbe611ecf52b6aaea9c26f37fdeae6060
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-03 14:29:12 +0200

    correct four rectangles

--- dhonffy/week-02/day-3/draw/fourRectangles.cpp
+++ dhonffy/week-02/day-3/draw/fourRectangles.cpp
@@ -1,5 +1,9 @@
 #include "draw.h"
 
+void setColor(SDL_Renderer *, int, int, int);
+
+void drawRectangle(SDL_Renderer *, int, int, int, int);
+
 void draw(SDL_Renderer *gRenderer)
 {
     // Exercise:
@@ -9,14 +13,47 @@ void draw(SDL_Renderer *gRenderer)
     int y1 = 50;
     int a = 30;
     int b = 25;
-    for (int i = 0; i < 4; ++i) {
-        SDL_SetRenderDrawColor(gRenderer, 0x20 + i * 32 /*R*/, 0x10 + i * 52 /*G*/, 0xA0 + i * 8 /*B*/, 0xFF /*A*/);
-        SDL_RenderDrawLine(gRenderer, (x1 + i * 60), (y1 + i * 80), (x1 + i * 60), (y1 + i * 80) + (a + i * 12));
-        SDL_RenderDrawLine(gRenderer, (x1 + i * 60), (y1 + i * 80) + (a + i * 12), (x1 + i * 60) + (b + i * 10),
-                           (y1 + i * 80) + (a + i * 12));
-        SDL_RenderDrawLine(gRenderer, (x1 + i * 60) + (b + i * 10), (y1 + i * 80) + (a + i * 12),
-                           (x1 + i * 60) + (b + i * 10), (y1 + i * 80));
-        SDL_RenderDrawLine(gRenderer, (x1 + i * 60) + (b + i * 10), (y1 + i * 80), (x1 + i * 60), (y1 + i * 80));
-    }
+    int red = 0x00;
+    int green = 0xff;
+    int blue = 0x88;
+    setColor(gRenderer, red, green, blue);
+    drawRectangle(gRenderer, x1, y1, a, b);
+    x1 = 150;
+    y1 = 150;
+    a = 50;
+    b = 55;
+    red = 0x20;
+    green = 0x4f;
+    blue = 0x08;
+    setColor(gRenderer, red, green, blue);
+    drawRectangle(gRenderer, x1, y1, a, b);
+    x1 = 250;
+    y1 = 250;
+    a = 90;
+    b = 85;
+    red = 0x90;
+    green = 0x0f;
+    blue = 0x68;
+    setColor(gRenderer, red, green, blue);
+    drawRectangle(gRenderer, x1, y1, a, b);
+    x1 = 350;
+    y1 = 350;
+    a = 100;
+    b = 25;
+    red = 0x10;
+    green = 0x00;
+    blue = 0xff;
+    setColor(gRenderer, red, green, blue);
+    drawRectangle(gRenderer, x1, y1, a, b);
 }
 
+void setColor(SDL_Renderer *gRenderer, int r, int g, int b)
+{
+    SDL_SetRenderDrawColor(gRenderer, r /*R*/, g /*G*/, b /*B*/, 0xFF /*A*/);
+}
+
+void drawRectangle(SDL_Renderer *gRenderer, int x1, int y1, int a, int b)
+{
+    SDL_Rect fillRect = {x1, y1, a, b};
+    SDL_RenderFillRect(gRenderer, &fillRect);
+}
