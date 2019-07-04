3ff347bc0f13e526d9c6bc418435a4633688736b
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-04 11:40:59 +0200

    do telephon book

--- dhonffy/week-02/day-1/telephoneBook/main.cpp
+++ dhonffy/week-02/day-1/telephoneBook/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <map>
+#include <string>
+
+
+//std::string jkmPhoneNumber();
 int main()
 {
 //    We are going to represent our contacts in a map where the keys are going to be strings and the values are going to be strings as well.
@@ -11,18 +15,29 @@ int main()
 //    Hortensia E. Foster 	606-481-6467
 //    Amanda D. Newland 	319-243-5613
 //    Brooke P. Askew 	    307-687-2982
-    std::map<std::string, std::string> phoneBook = {
-            {"William A. Lathan" "405-709-1865"},
-            {"John K. Miller" "402-247-8568"},
-            {"Hortensia E. Foster" "606-481-6467"},
-            {"Amanda D. Newland" "319-243-5613"},
-            {"Brooke P. Askew" "307-687-2982"}
+    std::map<std::string, std::string> phoneBook  = {
+            {"William A. Lathan", "405-709-1865"},
+            {"John K. Miller", "402-247-8568"},
+            {"Hortensia E. Foster", "606-481-6467"},
+            {"Amanda D. Newland", "319-243-5613"},
+            {"Brooke P. Askew", "307-687-2982"}
     };
 
 //    Create an application which solves the following problems.
 //    What is John K. Miller's phone number?
+    std::cout << phoneBook["John K. Miller"] << std::endl;
 //    Whose phone number is 307-687-2982?
+    for (auto it = phoneBook.begin(); it != phoneBook.end() ; ++it) {
+        if (it->second == "307-687-2982") {
+            std::cout << it->first << std::endl;
+        }
+    }
 //    Do we know Chris E. Myers' phone number?
+    if (phoneBook.find("Chris E. Myers") != phoneBook.end()) {
+        std::cout << "Yes." << std::endl;
+    } else {
+        std::cout << "No." << std::endl;
+    }
 
     return 0;
 }
