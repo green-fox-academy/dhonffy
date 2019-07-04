c34029adb593b938cc3def841d9305093b127985
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-04 15:36:44 +0200

    do product database 2 excercises in product database 1 project

--- dhonffy/week-02/day-1/productDatabase/main.cpp
+++ dhonffy/week-02/day-1/productDatabase/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 int searchPrice(std::map<std::string, int>, std::string);
+
 std::string mostExpensive(std::map<std::string, int>);
+
 std::string cheapest(std::map<std::string, int>);
+
 int sum(std::map<std::string, int>);
+
 float average(std::map<std::string, int>);
-int searchCheapProducts(std::map<std::string, int>, int);
+
+int amountOfCheapProducts(std::map<std::string, int>, int);
+
+std::vector<std::string> searchCheapProducts(std::map<std::string, int>, int);
+
+std::map<std::string, int> searchExpensiveProducts(std::map<std::string, int>, int);
+
 void searchExactPrice(std::map<std::string, int>, int);
 
 int main()
@@ -39,12 +50,24 @@ int main()
 //    What is the average price?
     std::cout << "The average price is: " << average(productDatabase) << std::endl;
 //    How many products' price is below 300?
-    std::cout << "There are " << searchCheapProducts(productDatabase, 300) << " products." << std::endl;
+    std::cout << "There are " << amountOfCheapProducts(productDatabase, 300) << " products." << std::endl;
 //    Is there anything we can buy for exactly 125?
     searchExactPrice(productDatabase, 125);
 //    What is the cheapest product?
     std::cout << cheapest(productDatabase) << " is the cheapest product." << std::endl;
-
+//    Which products cost less than 201? (just the name)
+    std::cout << "The products which are cheaper than 201, are:";
+    std::vector<std::string> cheapProductList = searchCheapProducts(productDatabase, 201);
+    for (int i = 0; i < cheapProductList.size(); ++i) {
+        std::cout << " " << cheapProductList[i];
+    }
+    std::cout << "." << std::endl;
+//    Which products cost more than 150? (name + price)
+    std::cout << "The products which are more expensive than 150, are:" << std::endl;
+    std::map<std::string, int> expensiveProductList = searchExpensiveProducts(productDatabase, 150);
+    for (auto it = expensiveProductList.begin(); it != expensiveProductList.end(); ++it) {
+        std::cout << it->first << ": " << it->second << std::endl;
+    }
     return 0;
 }
 
@@ -57,7 +80,7 @@ std::string mostExpensive(std::map<std::string, int> productDatabase)
 {
     int highestPrice = 0;
     std::string mostExpensive;
-    for (auto it = productDatabase.begin(); it != productDatabase.end() ; ++it) {
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
         if (it->second >= highestPrice) {
             highestPrice = it->second;
             mostExpensive = it->first;
@@ -70,7 +93,7 @@ std::string cheapest(std::map<std::string, int> productDatabase)
 {
     int lowestPrice = 2147483647;
     std::string cheapest;
-    for (auto it = productDatabase.begin(); it != productDatabase.end() ; ++it) {
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
         if (it->second <= lowestPrice) {
             lowestPrice = it->second;
             cheapest = it->first;
@@ -82,7 +105,7 @@ std::string cheapest(std::map<std::string, int> productDatabase)
 int sum(std::map<std::string, int> productDatabase)
 {
     int sum = 0;
-    for (auto it = productDatabase.begin(); it != productDatabase.end() ; ++it) {
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
         sum += it->second;
     }
     return sum;
@@ -93,22 +116,21 @@ float average(std::map<std::string, int> productDatabase)
     return static_cast<float> (sum(productDatabase)) / productDatabase.size();
 }
 
-int searchCheapProducts(std::map<std::string, int> productDatabase, int cheapPrice)
+int amountOfCheapProducts(std::map<std::string, int> productDatabase, int cheapPrice)
 {
     int amount = 0;
-    for (auto it = productDatabase.begin(); it != productDatabase.end() ; ++it) {
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
         if (it->second < cheapPrice) {
             amount++;
         }
     }
     return amount;
-
 }
 
 void searchExactPrice(std::map<std::string, int> productDatabase, int price)
 {
     bool valid = 0;
-    for (auto it = productDatabase.begin(); it != productDatabase.end() ; ++it) {
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
         if (it->second == price) {
             valid = 1;
         }
@@ -118,4 +140,27 @@ void searchExactPrice(std::map<std::string, int> productDatabase, int price)
     } else {
         std::cout << "There isn't." << std::endl;
     }
+}
+
+std::vector<std::string> searchCheapProducts(std::map<std::string, int> productDatabase, int price)
+{
+    std::vector<std::string> cheapProducts;
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
+        if (it->second < price) {
+            cheapProducts.push_back(it->first);
+        }
+    }
+    return cheapProducts;
+}
+
+std::map<std::string, int> searchExpensiveProducts(std::map<std::string, int> productDatabase, int price)
+{
+    std::map<std::string, int> cheapProducts;
+    for (auto it = productDatabase.begin(); it != productDatabase.end(); ++it) {
+        if (it->second > price) {
+            cheapProducts.insert(std::pair<std::string, int>(it->first, it->second));
+        }
+    }
+    return cheapProducts;
+
 }
