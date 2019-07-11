834a93d76472763ba5b5618ad620af8c8090856b
Honffy Daniel <honffy.daniel@gmail.com>
2019-07-11 11:23:06 +0200

    do pokemon

---
+++ dhonffy/week-03/day-4/pokemon/Pokemon.h
@@ -0,0 +1,22 @@
+#ifndef POKEMON_POKEMON_H
+#define POKEMON_POKEMON_H
+
+#include <iostream>
+
+class Pokemon
+{
+public:
+    Pokemon(const std::string &name, const std::string &type, const std::string &effectiveAgainst);
+
+    bool isEffectiveAgainst(Pokemon anotherPokemon);
+
+    std::string getName();
+
+
+private:
+    std::string _name;
+    std::string _type;
+    std::string _effectiveAgainst;
+};
+
+#endif //POKEMON_POKEMON_H
