882e6545468ee6e6afbf8ca1d147d48ccccb0787
Honffy Daniel <honffy.daniel@gmail.com>
2019-08-08 15:24:13 +0200

    follow slyleguide

--- dhonffy/week-06/day-02/computer/main.c
+++ dhonffy/week-06/day-02/computer/main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Use the Computer struct, give values to the fields and print them out in the main!
+// Use the computer struct, give values to the fields and print them out in the main!
 // Use the Notebook struct, give values to the fields and print them out in the main!
 
-struct Computer {
+struct computer {
     float cpu_speed_GHz;
     int ram_size_GB;
     int bits;
 };
 
-typedef struct {
+typedef struct notebook{
     float cpu_speed_GHz;
     int ram_size_GB;
     int bits;
-} Notebook;
+} notebook_t;
 
 int main()
 {
-    struct Computer computer;
+    struct computer computer;
     computer.cpu_speed_GHz = 1.2;
     computer.ram_size_GB = 8;
     computer.bits = 64;
 
-    printf("computer CPU speed %f GHz, RAM size %d GB, %d bits\n", computer.cpu_speed_GHz, computer.ram_size_GB,
+    printf("Computer CPU speed %f GHz, RAM size %d GB, %d bits\n", computer.cpu_speed_GHz, computer.ram_size_GB,
                                                                                          computer.bits);
 
-    Notebook notebook;
+    notebook_t notebook;
     notebook.cpu_speed_GHz = 2.1;
     notebook.ram_size_GB = 16;
     notebook.bits = 32;
 
-    printf("notebook CPU speed %f GHz, RAM size %d GB, %d bits\n", notebook.cpu_speed_GHz, notebook.ram_size_GB,
+    printf("Notebook CPU speed %f GHz, RAM size %d GB, %d bits\n", notebook.cpu_speed_GHz, notebook.ram_size_GB,
                                                                                          notebook.bits);
 
     return 0;
