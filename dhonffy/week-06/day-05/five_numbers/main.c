e075e33d5069d1f3ee699ea346542f4fdfa816e9
Honffy Daniel <honffy.daniel@gmail.com>
2019-08-09 17:42:34 +0200

    add five numbers in c

---
+++ dhonffy/week-06/day-05/five_numbers/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* read_integers(int);
+void print_values(int*, int);
+int main()
+{
+    // Create a program which accepts five integers from the console (given by the user)
+    // and store them in an array
+    // print out the memory addresses of the elements in the array
+    int amount = 5;
+    print_values(read_integers(amount), amount);
+    return 0;
+}
+
+int* read_integers(int amount)
+{
+    int* integers = (int*) malloc(amount * sizeof(int));
+    for (int i = 0; i < amount; ++i) {
+        printf("Type the %d. number!\n", i + 1);
+        scanf("%d", &integers[i]);
+    }
+    return integers;
+}
+
+void print_values(int* integers, int length)
+{
+    for (int i = 0; i < length; ++i) {
+        printf("Value %d: %d\n", i + 1, integers[i]);
+    }
+}
