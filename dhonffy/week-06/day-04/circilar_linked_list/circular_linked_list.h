b17667ecaccf9af94f080abcfe749ee5bcc83fe8
Honffy Daniel <honffy.daniel@gmail.com>
2019-08-27 20:27:28 +0200

    add circular linked list, not tested, at very begineing

---
+++ dhonffy/week-06/day-04/circilar_linked_list/circular_linked_list.h
@@ -0,0 +1,29 @@
+#ifndef LINKED_LIST_LINKED_LIST_H
+#define LINKED_LIST_LINKED_LIST_H
+
+typedef struct node{
+    int data;
+    struct node* next;
+} node_t;
+
+void add_head(node_t** head);
+void add_node_by_index(node_t** node, int index);
+void init(node_t** head, int size);
+void write(int data);
+
+
+/*
+void push_back(node_t** head, int data);
+void push_front(node_t** head, int data);
+void insert(node_t** head, int data, int element);
+void print(node_t* head);
+int size(node_t* head);
+int empty(node_t* head);
+void pop_front(node_t** head);
+int remove_if(node_t** head, int value);
+void delete_after(node_t** head, int element);
+node_t* search(node_t* head, int value);
+void sort_bubble(node_t* head);
+void sort_insertion(node_t* head);
+*/
+#endif //LINKED_LIST_LINKED_LIST_H
---
+++ dhonffy/week-06/day-04/circilar_linked_list/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "circular_linked_list.h"
+int main()
+{
+    node_t* head;
+    add_head(&head);
+    return 0;
+}
