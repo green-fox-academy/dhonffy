8e319fccd83c259bcedef86d4494f7fa3c616775
Honffy Daniel <honffy.daniel@gmail.com>
2019-09-04 11:41:59 +0200

    correct the values of the prescaler and counter period

--- dhonffy/week-09/day-01/dot_matrix_temperature_display/Src/tim.c
+++ dhonffy/week-09/day-01/dot_matrix_temperature_display/Src/tim.c
@@ -33,9 +33,9 @@ void MX_TIM2_Init(void)
   TIM_OC_InitTypeDef sConfigOC = {0};
 
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 42000;
+  htim2.Init.Prescaler = 42000-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 2000;
+  htim2.Init.Period = 2000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_OC_Init(&htim2) != HAL_OK)
