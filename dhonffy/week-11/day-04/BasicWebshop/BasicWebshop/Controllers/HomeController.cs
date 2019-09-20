e3245ef4c4ef30211e772d59589f4f4d8aa81e41
Honffy Daniel <honffy.daniel@gmail.com>
2019-09-20 17:06:38 +0200

    add basic webshop

---
+++ dhonffy/week-11/day-04/BasicWebshop/BasicWebshop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicWebshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/Show")]
+        public IActionResult Show()
+        {
+            List<ShopItem> list = new List<ShopItem>();
+            var shopItem = new ShopItem() { Name = "Running shoes", Description = "asdf", Price = 1000.0, QuantityOfStock = 5 };
+            list.Add(shopItem);
+            shopItem = new ShopItem() { Name = "Printer", Description = "assssdf", Price = 3000.0, QuantityOfStock = 2 };
+            list.Add(shopItem);
+            shopItem = new ShopItem() { Name = "Coca cola", Description = "asdéáéáf", Price = 25.0, QuantityOfStock = 0 };
+            list.Add(shopItem);
+
+            return View("Show", list);
+        }
+    }
+}
